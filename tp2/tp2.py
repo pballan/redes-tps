3f72fbf8a45d8ebe50a13b8a0750c5da8eadc3fd
julian flores <julian@MANDARINA>
2023-10-10 18:48:52 -0300

    tira un error

--- tp2/tp2.py
+++ tp2/tp2.py
@@ -4,8 +4,24 @@ from scapy.all import *
 from time import *
 from statistics import mean
 from itertools import product
+import requests
 
+###########
+url = "http://ipinfo.io/"
+miIP = "http://ipinfo.io/ip"
+###########
+def obtenerDatosAproximadosDe(ip):
+    #Esta funcion devuelve un json los datos a los
+    #que se puede acceder son: 'ip', 'city', 'region', 'country', 'loc', 'org', 'timezone'
+    #donde 'loc' son las coordenadas para ubicarlo en maps
+    request = requests.get( url + ip )
+    #supogo q no devuelve error la pagina
+    data = request.json()
+    return data
 
+def obtenerMiIp():
+    request = requests.get(miIP)
+    return request.text
 
 responses = {}
 #un diccionario de diccionarios
@@ -13,10 +29,11 @@ responses = {}
 
 _ips_recorridas = {}
 _intersaltos_por_ip = {}
+_intersaltos_filtrados = {}
 universidades = {
     'AmericaNorte': 'www.harvard.edu',
     'AmericaSur': 'www.uchile.cl',
-    'Europa': 'www,ox.ac.uk',
+    'Europa': 'www.ox.ac.uk',
     'Africa': 'www.unn.edu.org',
     'Asia': 'www.pku.edu.cn',
     'Oceania': 'www.anu.edu.au',
@@ -25,6 +42,7 @@ def imprimir(diccionario):
     for key, value in diccionario.items():
         print(key)
         print('\t', value)
+
 def obtenerMayorPorPromedio( ttl, intent ): #intent es diccionario { IP, [rtts] }
     if not intent or all( key == 'unans' for key in intent.keys() ):
         return
@@ -34,10 +52,31 @@ def obtenerMayorPorPromedio( ttl, intent ): #intent es diccionario { IP, [rtts] 
             key=lambda x: len(x[1]),default=None)
         _intersaltos_por_ip[ttl] = (max_pair[0], mean(max_pair[1]))
 
+
 def obtenerIntersaltosPorIP():
+    ttl_previo = 0
+    rtt_previo = 0
+    rtt_total = 0
+    ip_previo = obtenerMiIp()
+    _intersaltos_por_ip[ttl_previo] = (ip_previo, 0.0)
+    _intersaltos_filtrados[ttl_previo] = (ip_previo, 0.0)
+
     for ttl, intent in responses.items():
         #_intersaltos_por_ip[ttl] = []
         obtenerMayorPorPromedio( ttl, intent )
+
+    
+    for ttl in _intersaltos_por_ip:
+        if ttl != 0:
+            if _intersaltos_por_ip[ttl][1] > _intersaltos_por_ip[ttl_previo][1]:
+                _intersaltos_filtrados[ttl] = _intersaltos_por_ip[ttl]
+                ttl_previo = ttl
+    ttl_previo = 0
+    for ttl in _intersaltos_filtrados:
+        if ttl != 0:
+            #coord = obtenerDatosAproximadosDe( _intersaltos_filtrados[ttl][0] )["loc"]
+            _intersaltos_filtrados[ttl] = (_intersaltos_filtrados[ttl][0], _intersaltos_filtrados[ttl][1], _intersaltos_filtrados[ttl][1] - _intersaltos_filtrados[ttl_previo][1])#, coord  )
+            ttl_previo = ttl
 
 def obtenerIPsVisitadas():
     for ttl, intent in responses.items():
@@ -46,8 +85,8 @@ def obtenerIPsVisitadas():
             if ip != 'unans':
                 _ips_recorridas[ttl].append( ip )
 
-for ttl in range(1, 25):
-    for _ in range(5):
+for ttl in range(1, 30):
+    for _ in range(2):
         probe = IP(dst=sys.argv[1], ttl=ttl) / ICMP()
         t_i = time()
         ans = sr1(probe, verbose=False, timeout=0.8)
@@ -77,6 +116,8 @@ obtenerIPsVisitadas()
 imprimir( _ips_recorridas )
 obtenerIntersaltosPorIP()
 imprimir( _intersaltos_por_ip )
+imprimir(_intersaltos_filtrados)
+
 
 #calcular RTT entre cada salto para los que se recibe una respuesta time exceeded
 #diferenciar rutas
